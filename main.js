b92a4c3caec1411a1eb251ee9f0bf5faf8e9926c
VR-Sore-Neural <vrstoreneural@gmail.com>
2023-02-20 14:22:57 +0200

    area

--- main.js
+++ main.js
@@ -1,52 +1,35 @@
-// Variables - змінні
+// Example #1
 
-// let userName = "Саша";
+function sum() {
+    var name = "hosha";
+}
+
+console.log(name);
+
+// Example #2
+
+function sum() {
+    var name = "hosha";
+    console.log(name);
+}
+
+sum();
+
+// Блок кода
+{
+    // Объявляется фигурными скобками
+}
+
+// Функциональная область видимости
+// function sum() {
+    
+// }
+{
+    let userName = "Boss"
+    console.log(userName);
+
+    var someNumber = 10;
+}
 
 // console.log(userName);
-
-// Types of variables
-
-// Новий стандарт ES6:
-
-// Let - available
-
-// const - constanta
-
-// Старий стандарт ES5:
-
-// var - variable
-
-// Let - може змінювати значення під час роботи програми
-
-let userName = 'Sasha';
-
-console.log(userName);
-
-userName = 'Mark';
-
-console.log(userName);
-
-// const - не може змінювати значення
-
-const yearOfBirth = 1990;
-
-yearOfBirth = 2000; //(error)
-
-//--------------------------------------
-
-let name, age; //можна об'явити змінну, але не вказувати її значення 
-name = 'Yurij';
-
-const professor; // error - треба одразу вказани значення змінної
-professor = 'lapux';
-
-// var ------------
-// в отличие от Let and const имеет функциональную область видимостиб а не блочную
-// не рекомендується для використання
-// Може змінювати значення протягом роботи програми
-
-var city = 'Kyiv';
-city = 'Lviv'
-city = 'Donetsk';
-
-console.log(city);
+console.log(someNumber);
