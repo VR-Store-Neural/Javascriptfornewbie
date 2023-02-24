dab25494519e76d31cd5066f58376a27b9d6a825
VR-Store-Neural <vrstoreneural@gmail.com>
2023-02-24 12:39:43 +0200

    objects-this

--- main.js
+++ main.js
@@ -1,150 +1,103 @@
-// -------------Стрелочные функции-----------------
+// -------------Objects-----------------
 
-// function sayHi(name) {
-//     console.log(`Hi, ${name}! How are you?`); // decloratin
-// }
-// sayHi('Marge');
+// let userName = 'Mark';
+// let age = 33;
+// let isMarried = false;
 
-
-// стрелочный вариант
-// const SayHi2 = (name) => {
-//     console.log(`Hi, ${name}! How are you?`); 
-// }
-// SayHi2('Marchello');
-
-// стрелочный вариант, укороченный
-// const sayHi3 = (name) => console.log(`Hello, ${name}! What do you need?`);
-// sayHi3('Bobby');
-
-
-// function summ(a, b) {
-//     const result = a + b;
+// const person = {            // объявление свойств объекта
+//     userName: 'Mark',
+//     age: 33,
+//     isMarried: false,
+//     profession: 'Designer'
 // }
 
-// // стрелочный вариант
-// const summ = (a, b) => {
-//     const result = a + b;
-//     return result;
+// console.log(person);
+// console.log(person.userName);
+// console.log(person['age']);
+
+// let propertyName = 'profession';
+// console.log(person[propertyName]);
+
+// person.profession = 'JS developer'; // добавление свойств объекта
+// console.log(person);
+
+// delete person.age; // удаление свойств объекта
+// console.log(person);
+
+// ------------Методы в объектах------------
+
+// const person = {
+//     userName: 'Klara',
+//     age: 33,
+//     isMarried: false,
+//     sayHi: function () {
+//         console.log('Hello!');
+//     }
+// };
+
+// person.sayHi()
+
+// const human = {
+//     userName: 'Klara',
+//     age: 33,
+//     isMarried: false,
+//     sayHi: function () {
+//         console.log('Hello!');
+//     }
+// };
+
+// human.sayHi = function () {
+//     console.log('Hi-hi-hi!');
+// }
+// human.sayHi()
+
+// const girl = {
+//     userName: 'Klara',
+//     age: 33,
+//     isMarried: false,
+//     sayHi: function () {
+//         console.log('Hello!');
+//     }
+// };
+
+// girl.sayHi = function (name) {
+//     console.log(`Privet, ${name}!`);
+// }
+// girl.sayHi('Olena');
+
+// const people = {
+//     userName: 'Kiki',
+//     age: 21,
+//     isMarried: false,
+//     sayHi: function (name) {
+//         console.log(`Hello, ${name}!`);
+//     }
 // }
 
-// стрелочный вариант, укороченный
-// const summ = (a, b) => a + b;
-
-// const res = summ(10, 30);
-// console.log(res);
-
-// ------------------Массивы------------------
-
-// const autoBrands = ['Audi', 5, 'BMW', true, 'Mazda', 'Toyota', ['Vera', 'Nadejda', 'Halo'], 101];
-
-// console.log(autoBrands);
-
-// console.log(autoBrands[0]);
-
-// console.log(autoBrands.length); // возвращает длину массива, кол-во элементов
+// people.sayHi('Vasya')
 
 
-// -------------Методы массивов---------------
+// -------------Ключевое слово this-------------
 
-/*
-.push() - Додати елемент в кінець масиву(array)
-.pop() - Видалити елемент з кінця array
-.shift - Видалити елемент з початку array
-.unshift - Додати елемент з початку array
-.splice([start], [deleteCount, newElemets]) - 
-*/
-
-// const autoBrands = ['Audi', 'BMW', 'Mazda', 'Toyota'];
-// console.log(autoBrands);
-
-// autoBrands.push('Nissan');
-// console.log(autoBrands);
-
-// autoBrands = [1, 2, 45]; // --error
-
-// let array2 = autoBrands; //array2 == autoBrands - true
-// let array3 = ['Audi', 'BMW', 'Mazda', 'Toyota']; //array3 == autoBrands - false
-
-// autoBrands.splice(2, 1);
-// console.log(autoBrands);
-
-// ---------------------Циклы-----------------
-
-// console.log('Start');
-
-// for (let i = 0; i < 10; i++) {  // "i++" - увеличение на 1
-//     console.log(i);
-// }
-
-// console.log('Finish');
-
-// console.log('Start');
-
-// for (let i = 0; true; i++) {  // бесконечный цикл
-//     console.log(i);
-// }
-
-// console.log('Finish');
-
-// console.log('Start');
-
-// for (let i = 0; i < 10; i = i + 2) { 
-//     console.log(i); 
-// }
-
-// console.log('Finish');
-
-// --------------Обход массива циклом for-----------------
-
-// const autoBrands = ['Audi','BMW','Mazda','Toyota'];
-
-// console.log(autoBrands[0]);
-// console.log(autoBrands[1]);
-// console.log(autoBrands[2]);
-// console.log(autoBrands[3]);
-// console.log(autoBrands[4]);
-// console.log(autoBrands[5]);
-
-// for (m = 0; m < 5; m++) {
-//     console.log(autoBrands[m])
-//     console.log(m);
-// }
-
-// for (m = 0; m < autoBrands.length; m++) {
-//     console.log(autoBrands[m])
-//     console.log(m);
-// }
-
-// --------------Обход массива циклом for (of)-----------
-
-// const girls = ['masha', 'dasha', 'kasha', 'vasha', 'lasha'];
-
-// for (let girl of girls) {
-//     console.log(girl);
-// }
-
-// ------Обход массива методом forEach()------------
-
-const flowers = ['Rose', 'Dandelion', 'Sunflower', 'Chamomile'];
-
-flowers.forEach(function (flower, index) {
-    console.log(`${flower} => ${index}`);
-})
-
-console.log(`----------------`);
-
-flowers.forEach(printFlower);
-
-function printFlower (flower, i) {
-    console.log(`${flower} => ${i}`);
+const people = {
+    userName: 'Kiki',
+    age: 21,
+    isMarried: false,
+    sayHi: function (name) {
+        console.log(`Hello, ${name}! My name is ${people.userName}.`);
+    }
 }
 
-// flowers.forEach(printFlower());
+people.sayHi('Vasya');
 
-// function printFlower (flower, i) {
-//     console.log(`${flower} => ${i}`);
-// }
+const coala = {
+    userName: 'Kiki',
+    age: 21,
+    isMarried: false,
+    sayHi: function (name) {
+        console.log(this);
+        console.log(`Hello, ${name}! My name is ${this.userName}.`);
+    }
+}
 
-console.log(`----------------`);
-
-flowers.forEach((flower, i) => console.log(`${flower} => ${i}`));
+coala.sayHi('Vasya')
