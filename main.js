59ddadc5bcbc0117f307dfda87e2e635f875b94c
VR-Store-Neural <vrstoreneural@gmail.com>
2023-02-27 12:58:48 +0200

    event-listening

--- main.js
+++ main.js
@@ -1,25 +1,108 @@
-// ------Работа с CSS классами------
+// ------Работа с атрибутами------
 
 /*
-element.classList.add()
-.add()      добавляет CSS класс
-.remove()   удаляет CSS класс
-.toggle()   переключает CSS класс, то есть добавляет, если класс отсутствует и удаляет, если есть
-.contains() возвращает true/false, в зависимости есть такой класс или нет
+getAttribute(attrName) - повертає значення вказаного атрибуту
+setAttribute(name, value) - додає вказані атрибути та їх значення до елементу
+hasAttribute(attrName) повертає true, якщо елемент має вказаний атрибут
+removeAttribute(name, force) - видаляє вказаний атрибут
+
+toggleAttribute(name, force) - додає новий атрибут при відсутності чи видаляє існуючий атрибут
+hasAttribute() - повертає true, якщо елемент має які-небудь атрибут
+getAttributesNames() - повертає назви атрибутів елемента
 */
 
-// const heading = document.querySelector('h2');
-// console.log(heading);
+// const img = document.querySelector('#logo');
+// const srcValue = img.getAttribute('src');
+// console.log(srcValue);
 
-// heading.classList.add('red-text');
-// heading.classList.remove('red-text');
+// img.setAttribute('src', './img/php.png'); // change малюнок на інший з папки
+// img.setAttribute('width', '200'); // змінити розмір малюнку
 
-// heading.classList.toggle('green-text');
-// heading.classList.toggle('green');
+// img.src = './img/js.png'; // зміна малюнку(назад) на js.png
+// img.setAttribute('width', '50');
 
-const heading = document.querySelector('h2');
+// const button = document.querySelector('#button');
+// button.setAttribute('value', 'Slava Ukraine!')
+// button.value = "Text for button" // краще так - бо коротше
 
-const result = heading.classList.contains('green');   // результат contains записываем в переменную, и на основании этого выполняем то или иное условие
-if (result) {
-    
-}
+// ----------Working with event listening---------
+// -------- Робота з прослуховуванням подій--------- 
+
+// Будь-яка дія користувача є подією
+
+const button = document.querySelector('#button'); // знаходимо кнопку по id кнопки
+const img = document.querySelector('#logo'); // знаходимо малюнок по id лого
+
+button.value = 'delete';
+// button.addEventListener('click', function (){
+//     console.log('Click');                      // відловлюємо клік
+
+//     img.remove();                              // видаляє малюнок
+// })
+
+// button.onclick = function () {
+//     console.log('Click!');
+//     img.remove();
+// }
+
+
+// button.addEventListener('click', function (){
+//     console.log('Click');                     
+
+//     img.setAttribute('width', '50');               // зміна розміру малюнку по кліку              
+// })
+
+// addEventListener дозволяє додати декілька обробників для однієї події(event)
+// button.addEventListener('click', function (){
+//     console.log('Click-1');  
+// })     
+// button.addEventListener('click', function (){
+//     console.log('Click-3'); 
+// })
+// button.addEventListener('click', function (){
+//     console.log('Click-2');   
+// })             
+           
+// onclick - перезаписує значення (в данному випадку)методу
+
+// -----Відслідковування введення тексту у форму-----
+
+// const inputText = document.querySelector('#input-text');
+// const textBlock = document.querySelector('#text-block');
+
+// 1example
+// inputText.addEventListener('input', function () {
+//     // console.log('Input');
+//     console.log(inputText.value);
+//     textBlock.innerText = inputText.value;
+// })
+
+// 2example
+// inputText.addEventListener('input', () => {
+//     console.log(inputText.value);
+//     textBlock.innerText = inputText.value;
+// })
+
+// 3example
+// inputText.addEventListener('input', inputHandler);  // Якщо вставити скобки inputText.addEventListener('input', inputHandler());, працювать не буде, бо скобки одразу запустять цю функцію
+
+// function inputHandler () {
+//     console.log(inputText.value);
+//     textBlock.innerText = inputText.value;
+// }
+
+
+// --------------Об'єкт event---------------
+
+const list = document.querySelector('#list');
+
+// list.addEventListener('click', function () {
+//     console.log(this);
+// });
+
+// Дізнатись по якому саме об'єкту було здійснено клік
+list.addEventListener('click', function (event) {
+    console.log(this);
+    console.log(event);
+    console.log(event.target);
+})
