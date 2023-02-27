ec71ea4087c562a923d07223ef162fcc46bf1942
VR-Store-Neural <vrstoreneural@gmail.com>
2023-02-27 14:57:10 +0200

    setInterval-setTimeout

--- main.js
+++ main.js
@@ -1,66 +1,23 @@
-// ---------------ToDo список задач------------
+// ---------------setInterval------------
 
-// const todoList = document.querySelector('#todo-list');
-// const todoForm = document.querySelector('#todo-form');
-// const todoInput = document.querySelector('#todo-input');
+// const timerIntervalID = setInterval(function () {
+//     console.log('Fired!')
+// }, 2000);
 
-// todoForm.addEventListener('submit', formHandler);
+// clearInterval(timerIntervalID)
 
-// function formHandler(event) {
-//     event.preventDefault(); // відміна стандартної поведінки
-//     // console.log('Submit!');
+// ------------setTimeout-------------
 
-//     // Отримаємо назву задачі з поля вводу(input)
-//     const taskText = todoInput.value
-//     // console.log(taskText);
+// setTimeout(function () {
+//     console.log('Dodo')
+// }, 3000);
 
-//     // Створимо тег <li> через розмітку
-//     const li = `<li>${taskText}</li>`;
-//     // console.log(li);
+// --------setInterval + setTimeout-----------
 
-//     // Додаємо розмітку на сторінку
-//     todoList.insertAdjacentHTML('beforeend', li);
+const timerID = setInterval(function () {
+    console.log('Ogonb')
+}, 1000);
 
-//     // Очищення поля введення
-//     todoInput.value = '';
-
-//     // Перенесення focus на поле введення
-//     todoInput.focus();
-// }
-
-const todoList = document.querySelector('#todo-list');
-const todoForm = document.querySelector('#todo-form');
-const todoInput = document.querySelector('#todo-input');
-
-todoForm.addEventListener('submit', formHandler);
-
-function formHandler(event) {
-    event.preventDefault();
-    const taskText = todoInput.value;
-    
-    // Створюємо тег li за допомогою створення елементу. Плюсом цього способу є можливість додати event для цього елементу
-    const newTask = document.createElement('li');
-    newTask.innerText = taskText;
-    
-    // Створюємо кнопку Delete
-    const deleteBtn = document.createElement('button');
-    deleteBtn.setAttribute('role', 'button');
-    deleteBtn.innerText = 'Delete';
-    deleteBtn.style['margin-left'] = '15px';
-    newTask.append(deleteBtn);
-
-    // Додаємо подію на сторінку
-    // 1example
-    deleteBtn.addEventListener('click', function () {
-        this.closest('li').remove();
-    });
-    // 2example  
-    // deleteBtn.addEventListener('click', (event) => event.target.closest('li').remove());
-
-    // Додаємо елемент на сторінку
-    todoList.append(newTask);
-
-    todoInput.value = '';
-    todoInput.focus();
-}
-
+setTimeout(function () {
+    clearInterval(timerID)
+}, 10000)
